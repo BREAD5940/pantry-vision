656b265e4574a264ee669f7dbea1bf71a00eda89
mcm001 <matthew.morley.ca@gmail.com>
2019-01-01 18:07:11 -0800

    Add a picture, update accordingly

--- src/main/java/org/team5940/pantry/vision/processing/CalculateDistance.java
+++ src/main/java/org/team5940/pantry/vision/processing/CalculateDistance.java
@@ -7,6 +7,8 @@ package org.team5940.pantry.vision.processing;
  */
 
 public class CalculateDistance {
+  // double x, y, z;
+  double[] targetLoc = {0,0,0};
 
   /**
    * Calculate the horizontal distance to a target based
@@ -29,13 +31,20 @@ public class CalculateDistance {
    * expected angle of the goal.
    * @param camearPose
    * @param goalHeight known altitude of the goal above the ground
-   * @param currentAngle of the vision target
+   * @param currentAngle of the vision target in degrees
    * @return distance to the plane of the goal
    *   (basically the base of the triangle to the goal, with the
    *    hypotinuse as the straight line distance)
    */
   public double straightLineDistanceFromAngle(double[] cameraPose, double goalHeight, double currentAngle) {
-    return  (goalHeight - cameraPose[2]) / Math.sin(currentAngle);
+    return  (goalHeight - cameraPose[2]) / Math.toDegrees(Math.sin(currentAngle));
+  }
+
+  public double[] calculateXYZ(double[] cameraPose, double[] targetAngles, double goalDistance) {
+    targetLoc[0] = (Math.toDegrees(Math.cos(targetAngles[1])) * goalDistance) * (Math.toDegrees(Math.cos(targetAngles[0])));
+    targetLoc[1] = (Math.toDegrees(Math.cos(targetAngles[1])) * goalDistance) * (Math.toDegrees(Math.sin(targetAngles[0])));
+    targetLoc[2] = goalDistance * Math.toDegrees(Math.sin(targetAngles[1]));
+    return targetLoc;
   }
 
 }
